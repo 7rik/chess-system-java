7e4c440b31a7bb2963f038013f8857755dbdf1d8
7rik <bezerra.erik2003@gmail.com>
2023-08-29 15:09:11 -0300

    Placing pieces on the board

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,4 +1,5 @@
 package application;
+
 import chess.ChessPiece;
 
 public class UI {
@@ -11,7 +12,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println(" a b c d e f g h");
+		System.out.println("  a b c d e f g h");
 	}
 	
 	private static void printPiece(ChessPiece piece) {
@@ -19,7 +20,7 @@ public class UI {
 			System.out.print("-");
 		}
 		else {
-			System.out.println(piece);
+			System.out.print(piece);
 		}
 		System.out.print(" ");
 	}
