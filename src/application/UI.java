974f52c0b84e0622844e19b1d52e7e57b4260236
7rik <bezerra.erik2003@gmail.com>
2023-09-08 19:10:22 -0300

    Class Patch - Java 17

--- src/application/UI.java
+++ src/application/UI.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
-
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
